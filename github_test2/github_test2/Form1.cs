990df393b65a3647d43741220ae28499fa08ed4d
sung min Bae <44486582+sungminbae@users.noreply.github.com>
2022-03-21 16:38:26 +0900

    Update Form1.cs

--- github_test2/github_test2/Form1.cs
+++ github_test2/github_test2/Form1.cs
@@ -15,6 +15,9 @@ namespace github_test2
         public Form1()
         {
             InitializeComponent();
+            
+            
+            int test = 1;//new comment
         }
     }
 }
